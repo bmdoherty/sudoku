8f9dbdc9089eb3a8886f934fa3fb3a4d22a61de8
BM Doherty <bmdoherty@sidewinder.co.uk>
2017-11-11 13:09:01 +0000

    remove comments

--- model/House.js
+++ model/House.js
@@ -3,8 +3,6 @@ class House {
         this.id = id      
         this.cells = cells
         this.grid = grid
-        //this.used = new Set()
-        //this.unused = new Set([1,2,3,4,5,6,7,8,9])
 
         return this
     }  
